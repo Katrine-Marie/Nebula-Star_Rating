8f848872790390eec41c68a7f54da05c386a36c7
Katrine-Marie <fjordscene@gmail.com>
2019-03-04 11:19:08 +0100

    Base setup

---
+++ nebula-star-rating.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Plugin Name: Nebula Star Rating
+ * Description: Add 1 to 5 stars for any content with a simple plugin
+ * Version: 1.0
+ * Author: Katrine-Marie Burmeister
+ */
+
+ 
