9203bb77cfdbbccc012634be6e45732d3aebcec7
Katrine-Marie <fjordscene@gmail.com>
2019-03-04 11:22:13 +0100

    Updated file header w. license

--- nebula-star-rating.php
+++ nebula-star-rating.php
@@ -1,10 +1,12 @@
 <?php
 
-/*
- * Plugin Name: Nebula Star Rating
- * Description: Add 1 to 5 stars for any content with a simple plugin
- * Version: 1.0
- * Author: Katrine-Marie Burmeister
- */
-
- 
+/* **
+* Plugin Name: Nebula Star Rating
+* Description: Add 1 to 5 stars for any content, by means of a shortcode
+* Author: Katrine-Marie Burmeister
+* Version: 1.0.0
+* Author URI: https://fjordstudio.dk
+* Text Domain: nebula-rating
+* License:     GNU General Public License v3.0
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+** */
