1b3e742283720de7ab674c88b6efb7c6cd0021f8
Katrine-Marie <fjordscene@gmail.com>
2019-03-04 11:26:59 +0100

    Plugin basic setup

--- nebula-star-rating.php
+++ nebula-star-rating.php
@@ -16,3 +16,34 @@ namespace nebula\rating;
 if(!defined('ABSPATH')){
 	exit('Go away!');
 }
+
+add_shortcode('nebula-rating','nebula_add_stars');
+
+ function nebula_add_stars($atts)
+ {
+    $a = shortcode_atts( array(
+        'number' => '3',
+    ), $atts );
+
+	 $number_of_stars = $a['number'];
+
+	 if($number_of_stars=='1')
+	 {
+		 $bgcolor = '#F99';
+		 $icon 		= '<img width="40" src="http://www.freeiconspng.com/uploads/warning-icon-5.png"/>';
+	 } elseif($number_of_stars=='2' || $number_of_stars=='3') {
+		 $bgcolor = '#FF0';
+		$icon = '';
+	 } else {
+		 $bgcolor = '#9F9';
+		 $icon 		= '<img width="40" src="https://cdn0.iconfinder.com/data/icons/round-ui-icons/128/tick_green.png"/>';
+	 }
+
+	 return ("
+	 <div style='padding:10px; border-radius:5px; font-size:16pt; background:$bgcolor; width:100%; margin-bottom:20px;'>
+	 $icon
+	 This is Rated:
+	 $number_of_stars out of 5 stars
+	 </div>
+	 ");
+ }
