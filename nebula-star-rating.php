831a0b2bd1ff0fd473d7f65c2443b66da4111d4d
Katrine-Marie <fjordscene@gmail.com>
2019-03-06 10:31:00 +0100

    Adjusted colour

--- nebula-star-rating.php
+++ nebula-star-rating.php
@@ -36,7 +36,7 @@ class ratingShortcode{
  		 $bgcolor = '#F99';
  		 $icon 		= '<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="#a10014" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12" y2="17"></line></svg>';
  	 } elseif($number_of_stars=='2' || $number_of_stars=='3') {
- 		 $bgcolor = '#FF0';
+ 		 $bgcolor = '#ffe972';
  		$icon = '';
  	 } else {
  		 $bgcolor = '#9F9';
